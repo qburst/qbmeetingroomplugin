79b22553fc4d76fb4aaeb164da8fd4d5bfe850ab
Tobias Droste <tdroste@gmx.de>
2015-04-22 22:54:54 +0200

    Add compatibility with rails >= 4
    Rails 4 disallows 'match' in routes.rb without specifying the http method.
    Replace all 'match' with 'get'.
    
    Signed-off-by: Tobias Droste <tdroste@gmx.de>

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-match 'meeting_room_calendar/', :to => 'meeting_room_calendar#index'
-match 'meeting_room_calendar/:project_id', :to => 'meeting_room_calendar#index', :project_id => /\d+/
-match 'meeting_room_calendar/index', :to => 'meeting_room_calendar#index'
-match 'meeting_room_calendar/index/:project_id', :to => 'meeting_room_calendar#index', :project_id => /\d+/
-match 'meeting_room_calendar/missing_config', :to => 'meeting_room_calendar#missing_config'
-match 'meeting_room_calendar/create', :to => 'meeting_room_calendar#create'
-match 'meeting_room_calendar/update', :to => 'meeting_room_calendar#update'
-match 'meeting_room_calendar/delete', :to => 'meeting_room_calendar#delete'
+get 'meeting_room_calendar/', :to => 'meeting_room_calendar#index'
+get 'meeting_room_calendar/:project_id', :to => 'meeting_room_calendar#index', :project_id => /\d+/
+get 'meeting_room_calendar/index', :to => 'meeting_room_calendar#index'
+get 'meeting_room_calendar/index/:project_id', :to => 'meeting_room_calendar#index', :project_id => /\d+/
+get 'meeting_room_calendar/missing_config', :to => 'meeting_room_calendar#missing_config'
+get 'meeting_room_calendar/create', :to => 'meeting_room_calendar#create'
+get 'meeting_room_calendar/update', :to => 'meeting_room_calendar#update'
+get 'meeting_room_calendar/delete', :to => 'meeting_room_calendar#delete'
